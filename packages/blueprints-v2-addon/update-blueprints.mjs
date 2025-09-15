1d60e8e0139c56f5d1d5d2d16625693a16486bfa
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-15 06:48:16 +0200

    chore: Updated blueprints version

--- packages/blueprints-v2-addon/update-blueprints.mjs
+++ packages/blueprints-v2-addon/update-blueprints.mjs
@@ -6,7 +6,7 @@ import gitDiffApply from 'git-diff-apply';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
-const CURRENT_VERSION = '1.1.4';
+const CURRENT_VERSION = '1.1.5';
 
 async function updateBlueprints({ from, to }) {
   const startTag = from;

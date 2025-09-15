7abf7ddc71e1a2903b0ef58c34f77a4b72fc00c2
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-15 06:31:13 +0200

    chore: Removed analyze-ember-project-dependencies

--- packages/analyze-ember-project-dependencies/src/utils/find-dependencies/analyze-template.ts
+++
@@ -1,9 +0,0 @@
-import type { PackageAnalysis } from '../../types/index.js';
-import { findComponentsHelpersModifiers } from './analyze-template/index.js';
-import type { Data } from './index.js';
-
-export function analyzeTemplate(file: string, data: Data): PackageAnalysis {
-  const results = findComponentsHelpersModifiers(file, data);
-
-  return results;
-}

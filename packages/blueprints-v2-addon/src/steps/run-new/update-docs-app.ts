e1c1e868de0d4bbc1eff7868c2b2b3d50195517e
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-12-10 14:49:56 +0100

    breaking, bugfix: Supported Vite for docs-app (blueprints-v2-addon)

--- packages/blueprints-v2-addon/src/steps/run-new/update-docs-app.ts
+++ packages/blueprints-v2-addon/src/steps/run-new/update-docs-app.ts
@@ -2,10 +2,7 @@ import { existsSync } from 'node:fs';
 import { join } from 'node:path';
 
 import type { Options } from '../../types/run-new.js';
-import {
-  updateEmberCliBuild,
-  updatePackageJson,
-} from './update-docs-and-test-apps/index.js';
+import { updatePackageJson } from './update-docs-and-test-apps/index.js';
 
 export function updateDocsApp(options: Options): void {
   const { docsApp, projectRoot } = options;
@@ -16,6 +13,5 @@ export function updateDocsApp(options: Options): void {
     return;
   }
 
-  updateEmberCliBuild(appRoot, options);
   updatePackageJson(appRoot, options);
 }

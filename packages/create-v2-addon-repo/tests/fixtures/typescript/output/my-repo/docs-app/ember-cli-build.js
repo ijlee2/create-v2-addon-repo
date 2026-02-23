de1e1fc9088268220ce037144049a97fc62dbeea
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-02-23 15:21:49 +0100

    bugfix: Updated blueprints

--- packages/create-v2-addon-repo/tests/fixtures/typescript/output/my-repo/docs-app/ember-cli-build.js
+++ packages/create-v2-addon-repo/tests/fixtures/typescript/output/my-repo/docs-app/ember-cli-build.js
@@ -3,10 +3,6 @@
 const { compatBuild } = require('@embroider/compat');
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-function isProduction() {
-  return EmberApp.env() === 'production';
-}
-
 module.exports = async function (defaults) {
   const { buildOnce } = await import('@embroider/vite');
 
@@ -14,60 +10,7 @@ module.exports = async function (defaults) {
     'ember-cli-babel': {
       enableTypeScriptTransform: true,
     },
-
-    // Add options here
   });
 
-  const options = {
-    packagerOptions: {
-      cssLoaderOptions: {
-        modules: {
-          localIdentName: isProduction()
-            ? '[sha512:hash:base64:5]'
-            : '[path][name]__[local]',
-          mode: (resourcePath) => {
-            // We want to enable the local mode only for our own host app.
-            // All other addons should be loaded in the global mode.
-            const hostAppLocation =
-              'docs-app/node_modules/.embroider/rewritten-app';
-
-            return resourcePath.includes(hostAppLocation) ? 'local' : 'global';
-          },
-        },
-        sourceMap: !isProduction(),
-      },
-      publicAssetURL: '/',
-      webpackConfig: {
-        module: {
-          rules: [
-            {
-              test: /(node_modules\/\.embroider\/rewritten-app\/)(.*\.css)$/i,
-              use: [
-                {
-                  loader: 'postcss-loader',
-                  options: {
-                    sourceMap: !isProduction(),
-                    postcssOptions: {
-                      config: './postcss.config.js',
-                    },
-                  },
-                },
-              ],
-            },
-            /*
-              Uncomment this rule to load asset files, e.g. fonts, icons, etc.
-              See https://webpack.js.org/guides/asset-modules/ for more information.
-            */
-            // {
-            //   test: /(node_modules\/\.embroider\/rewritten-app\/)(.*\.(ttf|woff))$/,
-            //   type: 'asset/resource',
-            // },
-          ],
-        },
-      },
-    },
-    splitAtRoutes: [],
-  };
-
-  return require('@embroider/compat').compatBuild(app, buildOnce, options);
+  return compatBuild(app, buildOnce);
 };
--- packages/create-v2-addon-repo/tests/fixtures/typescript/output/my-repo/docs-app/vite.config.mts
+++ packages/create-v2-addon-repo/tests/fixtures/typescript/output/my-repo/docs-app/vite.config.mts
@@ -3,6 +3,11 @@ import { babel } from '@rollup/plugin-babel';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  css: {
+    modules: {
+      generateScopedName: 'docs-app__[hash]-[local]',
+    },
+  },
   plugins: [
     classicEmberSupport(),
     ember(),
